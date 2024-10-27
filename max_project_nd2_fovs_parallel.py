37021f01ec5252ba98405b09b782d892c7b7837f
Christopher Workman <cdiworkman@gmail.com>
2024-10-27 16:36:08 -0400

    Update max_project_nd2_fovs_parallel.py
    It's faster to read serially with PIMs and max project batches of FOVs.

--- max_project_nd2_fovs_parallel.py
+++ max_project_nd2_fovs_parallel.py
@@ -1,78 +1,80 @@
 from tifffile import imwrite
 import numpy as np
-from nd2reader import ND2Reader
+import pims
 from concurrent.futures import ThreadPoolExecutor, as_completed
 import time
 import sys
 import psutil
 import os
 
-# Function to process a single FOV and return the result
-def process_fov(v, in_filename, all_fovs_array):
-    with ND2Reader(in_filename) as images:
-        # Set the iterating and bundling axes for ND2Reader within each thread
-        images.iter_axes = 'v'
-        images.bundle_axes = 'czyx'
-        
-        # Retrieve the FOV by index, process it, and perform max projection
-        fov = images[v].astype(np.uint16)
-        print(f"Processing FOV {v} with shape: {fov.shape}")
-
-        all_fovs_array[v] = np.max(fov, axis=1)  # Max project and directly store in the shared array
+# Function to max project a single FOV
+def process_fov(fov, m, all_fovs_array):
+    all_fovs_array[m] = np.max(fov, axis=1)  # Max projection across the Z-axis
 
 # Main function with input and output filenames as arguments
 def main(in_filename, out_filename, num_fovs=None):
     # Start total duration timer
     start_total = time.time()
 
-    # Open ND2Reader to access input images
-    with ND2Reader(in_filename) as images:
+    # Open pims with ND2 file to get image dimensions
+    with pims.open(in_filename) as images:
         # Start timer for reading dimensions
         start_reading = time.time()
-        
+
         # Get image dimensions
         channels = images.sizes['c']
         width = images.sizes['x']
         height = images.sizes['y']
-        fovs = images.sizes['v']
+        fovs = images.sizes['m']
         zs = images.sizes['z']
-        
+
+        images.iter_axes = 'm'
+        images.bundle_axes = 'czyx'
+
         # Limit FOVs if specified
         if num_fovs:
             fovs = min(num_fovs, fovs)
-        
+
         # Stop timer for reading dimensions
         duration_reading = time.time() - start_reading
         print(f"Time to read dimensions: {duration_reading:.2f} seconds")
-        
+
         # Preallocate the results array to hold the selected FOVs: shape (fovs, channels, height, width)
         all_fovs_array = np.zeros((fovs, channels, height, width), dtype=np.uint16)
 
-        # Start timer for processing FOVs
-        start_processing = time.time()
-
-        # Memory per thread (adjust based on testing)
-        overhead = 5
+        # Calculate memory-per-thread, set max_workers based on memory and CPU count
+        overhead = 1.5
         data_type_size = 2  # bytes for uint16
-        memory_per_thread = height * width * channels * zs * data_type_size * overhead # in bytes
+        memory_per_thread = height * width * channels * zs * data_type_size * overhead  # in bytes
         print('Estimated memory per thread ', memory_per_thread / 1e9, ' GB')
         available_memory = psutil.virtual_memory().available
         print('Available Memory', available_memory / 1e9, 'GB')
         optimal_workers = min(available_memory // memory_per_thread, os.cpu_count())
         print('Optimal Workers', optimal_workers)
-        max_workers = max(1 , optimal_workers)  # Ensure at least 1 worker
+        max_workers = max(1, optimal_workers)  # Ensure at least 1 worker
 
-        # Parallel processing of each FOV with ordered writing
-        with ThreadPoolExecutor(max_workers=max_workers) as executor:
-            # Submit tasks to process each FOV and store futures with their respective indices
-            futures = {executor.submit(process_fov, v, in_filename, all_fovs_array): v for v in range(fovs)}  
+        # Start timer for processing FOVs
+        start_processing = time.time()
 
-            # Collect results as each FOV completes
-            for future in as_completed(futures):
-                # v, fov_max = future.result()
-                future.result()
-                # all_fovs_array[v] = fov_max  # Store result by FOV index for ordered writing
+        # Loop through FOVs in batches
+        batch_size = max_workers
+        for start_index in range(0, fovs, batch_size):
+            start_batch = time.time()
+            end_index = min(start_index + batch_size, fovs)
+            batch = [images[m] for m in range(start_index, end_index)]
 
+            duration_load = time.time() - start_batch
+            print(f"Time to process FOVs {start_index} to {end_index}: {duration_load:.2f} seconds")
+
+            # Process the current batch in parallel
+            with ThreadPoolExecutor(max_workers=max_workers) as executor:
+                futures = {executor.submit(process_fov, batch[i], start_index + i, all_fovs_array) for i in range(len(batch))}
+
+                # Wait for all futures in the batch to complete
+                for future in as_completed(futures):
+                    future.result()
+            duration_batch = time.time() - start_batch
+            print(f"Time to process FOVs: {duration_batch:.2f} seconds")
         # Stop timer for processing FOVs
         duration_processing = time.time() - start_processing
         print(f"Time to process FOVs: {duration_processing:.2f} seconds")
